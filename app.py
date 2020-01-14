50b6f7750c0a859359f1e298e63c7cd134de0e98
naga oleti <venu.babu80@gmail.com>
2020-01-14 23:09:25 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://stackoverflow.com/questions")
+soup = BeautifulSoup(response.text, "html.parser")
+questions = soup.select(".question-summary")
+for question in questions:
+    print(question.select_one(".question-hyperlink").getText())
